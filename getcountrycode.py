ce4a5bcb13311a477781478c7b75986044b8e05d
NatalieZawadi <nataliezawadiwesula@gmail.com>
2025-06-07 20:37:50 +0300

    get country code

---
+++ getcountrycode.py
@@ -0,0 +1,8 @@
+country_code = {'india':'0091',
+                'Australia':'0025',
+                'Nepal':'00977',
+            }
+print("Contry code for India - ")
+print(country_code.get('india', 'Not Found'))
+print("Contry code for Japan - ")
+print(country_code.get('japan', 'Not Found'))
